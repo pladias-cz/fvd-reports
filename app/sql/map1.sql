f2b1b6ca47619ba2873fbc5111f1a708769d8849
petr <krkabol@gmail.com>
2026-09-22 15:24:18 +0200

    modify queries to include austria-only taxa in counting

--- app/sql/map1.sql
+++ app/sql/map1.sql
@@ -7,12 +7,12 @@ FROM (SELECT s.geom_wgs                                                as geom,
               WHERE rec.validation_status IN (0, 4)
                 AND ST_Intersects(rec.coords_wgs, s.geom_wgs))         as pladias_2,
 
-             --Q2 union, that is unique taxa across both sources
-             (SELECT count(DISTINCT tt.pladias_taxon_id)
-              FROM gbif.records gbif
-                       JOIN gbif.taxa tt ON (tt.col_id = gbif.taxon_col_id)
-              WHERE ST_Intersects(gbif.coords, s.geom_wgs))            as gbif_2,
+            -- (SELECT count(DISTINCT tt.pladias_taxon_id)
+             -- FROM gbif.records gbif
+             --          JOIN gbif.taxa tt ON (tt.col_id = gbif.taxon_col_id)
+             -- WHERE ST_Intersects(gbif.coords, s.geom_wgs))            as gbif_2,
 
+             --Q2 union, that is unique taxa across both sources
              (SELECT COUNT(*) AS total_unique_taxa
               FROM (SELECT rec.taxon_id AS taxon
                     FROM atlas.records rec
@@ -21,7 +21,7 @@ FROM (SELECT s.geom_wgs                                                as geom,
 
                     UNION
 
-                    SELECT tt.pladias_taxon_id
+                    SELECT COALESCE(tt.pladias_taxon_id, tt.id + 5000000) --to cover also austria-only taxa with NULL in pladias_taxon_id
                     FROM gbif.records gbif
                              JOIN gbif.taxa tt ON (tt.col_id = gbif.taxon_col_id)
                     WHERE ST_Intersects(gbif.coords, s.geom_wgs)) sub) as total
